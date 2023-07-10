7d8823eeaec40caf23b1dbcae39a0d774454168d
Mae Miller <maeborow@posteo.net>
2023-07-09 22:38:34 -0700

    Working state!

--- gifmak3r/main.py
+++ gifmak3r/main.py
@@ -12,11 +12,11 @@ args = parser.parse_args()
 
 if validators.url(args.video):
     URLS = [args.video]
-    if os.path.exists("result/inter.mkv"):
-        os.remove("result/inter.mkv")
+    if os.path.exists("result/inter.mp4"):
+        os.remove("result/inter.mp4")
     ydl_opts = {'final_ext': 'mkv',
                 'download_ranges': yt_dlp.utils.download_range_func([], [[args.start, args.end]]),
-                'format': 'bv*[ext=mp4]/b[ext=mp4] / bv*+ba/b',
+                'format': 'bv*[ext=mp4]+ba[ext=vorbis]/b[ext=mp4] / bv*+ba/b',
                 'outtmpl': {'default': 'result/inter.mp4'}}
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -26,7 +26,7 @@ else:
     stream = ffmpeg.input(args.video)
     
         
-# stream = ffmpeg.input('result/inter.mp4')
+# stream = ffmpeg.input('result/inter.mkv')
 stream = ffmpeg.filter(stream, 'scale', height=300, width=-2).output('result/inter-scale.mp4').run(overwrite_output=True)
 stream = ffmpeg.input('result/inter-scale.mp4').filter('palettegen', stats_mode='full').output('result/palettegen_full.png')
 stream = ffmpeg.run(stream, overwrite_output=True)
